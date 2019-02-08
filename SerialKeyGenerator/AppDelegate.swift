ffae93eb6f2481dea3d46dc090960a5f9c21d6c5
Jin HengJun <jinhengjun@jins-mac.local>
2019-02-08 07:44:35 -0800

    Initial Commit

---
+++ SerialKeyGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialKeyGenerator
+//
+//  Created by Jin HengJun on 2/8/19.
+//  Copyright © 2019 Jin HengJun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SerialKeyGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialKeyGenerator
+//
+//  Created by Jin HengJun on 2/8/19.
+//  Copyright © 2019 Jin HengJun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
